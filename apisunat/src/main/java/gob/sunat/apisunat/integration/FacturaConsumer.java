7228dbca1c7708a03020902ac90689fdb556b9a3
Fred <fduartej@gmail.com>
2021-09-04 17:05:04 -0500

    Fix de Consumer

--- apisunat/src/main/java/gob/sunat/apisunat/integration/FacturaConsumer.java
+++ apisunat/src/main/java/gob/sunat/apisunat/integration/FacturaConsumer.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.core.Message;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gob.sunat.apisunat.model.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Service
 public class FacturaConsumer {
 
